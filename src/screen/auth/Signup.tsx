6e003e043db46dbd6ee57d76dd3372515d9336d2
shubham sarode <shubham.sarode@extwebtech.in>
2023-08-13 11:07:30 +0530

    working on signup page

---
+++ src/screen/auth/Signup.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface SignupProps {}
+
+interface SignupState {}
+
+class Signup extends React.Component<SignupProps, SignupState> {
+  constructor(props: SignupProps) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <></>;
+  }
+}
+
+export default Signup;
