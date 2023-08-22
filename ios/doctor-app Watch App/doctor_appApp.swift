1597525fc8177116b817ed29e9b719bd0e86548f
shubham sarode <shubham.sarode@extwebtech.in>
2023-08-22 17:44:39 +0530

    created app for watch

---
+++ ios/doctor-app Watch App/doctor_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  doctor_appApp.swift
+//  doctor-app Watch App
+//
+//  Created by Shubham sarode on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct doctor_app_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
